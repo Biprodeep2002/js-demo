1ebc3b62f4545cf9ead9cb50fc8d91981cbcce22
Biprodeep2002 <123178532+Biprodeep2002@users.noreply.github.com>
2025-04-03 06:03:15 +0000

    first commit

---
+++ basic/convo.js
@@ -0,0 +1,9 @@
+let score="33"
+console.log(typeof score)
+
+let c=Number(score)
+console.log(typeof c)
+
+let isNum=5
+let isBool=Boolean(isNum)
+console.log(isBool);
---
+++ basic/variables.js
@@ -0,0 +1,13 @@
+const a=1
+let b="abc@gmail.com"
+var c="789"
+d="jaipur"
+let e 
+
+// a=2
+b="xyz@yopmail.com"
+c="123"
+d="Kolkata"
+
+console.log(a)
+console.table([a,b,c,d,e])
